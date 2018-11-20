efd8516e20d79ab0d28c39e5f3dd3cafa709c69d
satya19051997 <satya.nandy@evossolutions.com>
2018-11-20 16:32:51 +0530

    Adding UnitOfMeasure Entity OneToOne Relationship Without CASCADING

--- src/main/java/guru/springframework/models/Ingredient.java
+++ src/main/java/guru/springframework/models/Ingredient.java
@@ -3,10 +3,12 @@ package guru.springframework.models;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Ingredient 
@@ -19,6 +21,10 @@ public class Ingredient
 	
 	@ManyToOne
 	private Recipe recipe;
+	
+	@OneToOne(fetch = FetchType.EAGER)
+	private UnitOfMeasures usnitOfMeasure;
+	
 	public Recipe getRecipe() {
 		return recipe;
 	}
